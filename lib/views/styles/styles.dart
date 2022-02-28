b95a435b7db8dd62e5d2da65e0d7bcf9a417a5fd
Ali Hamza <ahmadhamzanaqvi@gmail.com>
2022-03-01 01:44:47 +0500

    topbar

---
+++ lib/views/styles/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xffF9F9F9);
+
+
+const TextStyle home=TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  
+);
